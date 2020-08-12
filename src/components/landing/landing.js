b8c565ad839d504ff0a480068a7cc11ee271e9b5
Abhishyam <abhishyamnayak@gmail.com>
2020-08-13 00:34:47 +0530

    added a delayed fadeIn to name banner

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -4,18 +4,49 @@ import styles from './landing.module.scss';
 import '../../index.css'
 
 class landing extends React.Component{
-
+  // <div className={styles.bannercontainer}>
+  // <div className={styles.bannerNamecontainer}>
+  // <div className={[styles.neonwrapper,styles.landing].join(' ')}>
+  //  <span >{this.props.name}</span>
+  // </div>
+  // <span className={styles.bannername}>{this.props.name}</span>
+  // <span className={styles.bannername}>{this.props.name}</span>
+  // <span className={styles.bannername}></span>
+  // <span className={styles.bannername}></span>
+  // </div>
+  //
+  //
+  //
+  // <div className={styles.links}>
+  // <a href="#">
+  // <span></span>
+  // <span></span>
+  // <span></span>
+  // <span></span>
+  // Coder
+  // </a>
+  //   <a href="#">
+  //   <span></span>
+  //   <span></span>
+  //   <span></span>
+  //   <span></span>
+  //   Photographer
+  //   </a>
+  //   </div>
+  // </div>
 
   render(){
     return(
-      <div className={styles.bannercontainer}>
-
-      <div className={[styles.neonwrapper,styles.landing].join(' ')}>{this.props.name}</div>
-      <span className={styles.bannername}>{this.props.name}</span>
-      <span className={styles.bannername}>{this.props.name}</span>
-      <span className={styles.bannername}></span>
-      <span className={styles.bannername}></span>
+    <div className={styles.landingContainer}>
+    <div className={styles.bannerName}>{this.props.name}</div>
+    <span className={styles.bannerName}>{this.props.name}</span>
+    <span className={styles.bannerName}>{this.props.name}</span>
+    <span className={styles.bannerName}></span>
+    <span className={styles.bannerName}></span>
       <div className={styles.links}>
+      <span className={styles.bannerBorder}></span>
+      <span className={styles.bannerBorder}></span>
+      <div className ={styles.linksContainer}>
       <a href="#">
       <span></span>
       <span></span>
@@ -30,10 +61,14 @@ class landing extends React.Component{
         <span></span>
         Photographer
         </a>
-        </div>
       </div>
 
-    )
+      </div>
+
+    </div>
+
+
+    );
   }
 }
 
